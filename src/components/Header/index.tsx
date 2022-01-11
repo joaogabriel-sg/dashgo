9a7fce27bf7671fc95d9d5c4f6cb70f12eab38ea
João Gabriel <main.joaogabrielsg@gmail.com>
2022-01-11 15:36:51 -0300

    feat: add responsive sidebar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,7 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
+import { RiMenuLine } from "react-icons/ri";
+
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
 
 import { Logo } from "./Logo";
 import { NotificationsNav } from "./NotificationsNav";
@@ -6,6 +9,8 @@ import { Profile } from "./Profile";
 import { SearchBox } from "./SearchBox";
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer();
+
   const isWideVersion = useBreakpointValue({ base: false, lg: true });
 
   return (
@@ -19,6 +24,17 @@ export function Header() {
       px="6"
       align="center"
     >
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          mr="2"
+          variant="unstyled"
+          onClick={onOpen}
+        ></IconButton>
+      )}
+
       <Logo />
 
       {isWideVersion && <SearchBox />}
