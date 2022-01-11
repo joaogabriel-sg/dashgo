4cb966dd9edcf623354000c1bf3bf024d65d4d52
João Gabriel <main.joaogabrielsg@gmail.com>
2022-01-11 15:12:24 -0300

    feat: add responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Flex, Box, Avatar, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Diego Fernandes</Text>
-        <Text color="gray.300" fontSize="small">
-          main.joaogabriel@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Diego Fernandes</Text>
+          <Text color="gray.300" fontSize="small">
+            main.joaogabriel@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size="md"
